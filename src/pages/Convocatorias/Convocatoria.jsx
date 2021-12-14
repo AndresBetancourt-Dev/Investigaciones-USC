6b9a2567e3e06137efb11f0142744426250c157d
Andrés David Betancourt Santana <65095693+AndresBetancourt-Dev@users.noreply.github.com>
2021-12-14 03:43:09 -0500

    🎈 Otri, Editorial Pages Re-Done

---
+++ src/pages/Convocatorias/Convocatoria.jsx
@@ -0,0 +1,16 @@
+import PageLayout from "components/PageLayout";
+import SEO from "components/SEO";
+import React from "react";
+
+const Convocatoria = () => {
+  return (
+    <PageLayout title={"Convocatorias"}>
+      <SEO
+        title="Convocatorias - Dirección General de Investigaciones"
+        description="La Dirección General de Investigaciones en esta página muestras las resoluciones y circulares normativas que han sido publicadas."
+      />
+    </PageLayout>
+  );
+};
+
+export default Convocatoria;
